c2e2ffd80822122693be77d5536321fb4afb4898
Davis Archibald <davisarchibald@Daviss-MacBook-Pro.local>
2017-03-23 07:54:08 -0600

    build out basic structure

---
+++ react/src/js/components/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Feed from '../feed';
+
+export default function App(props) {
+    return (
+        <section>
+            {/* <Nav /> */}
+            <Feed />
+            {/* <Footer /> */}
+        </section>
+    );
+}
--- react/src/js/index.js
+++ react/src/js/index.js
@@ -1,1 +1,27 @@
-// app code goes here
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { Router, Route, browserHistory } from 'react-router';
+import { routerMiddleware } from 'react-router-redux';
+import Reducer from './components/reducers';
+import App from './components/app';
+
+const initialState = {};
+const reduxRouterMiddleware = routerMiddleware(browserHistory);
+const newCreateStore = compose(
+    applyMiddleware(thunk, reduxRouterMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
+const store = newCreateStore(Reducer, initialState);
+
+render(
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route component={App} path="/" />
+        </Router>
+    </Provider>,
+  document.getElementById('app')
+);
