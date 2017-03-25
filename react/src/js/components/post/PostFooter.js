345a7c1c8ea9c0b9b0bb82a47c2c1344d97c6d7f
Davis Archibald <davisarchibald@Daviss-MacBook-Pro.local>
2017-03-25 11:04:53 -0600

    convert post footer into a class, need state and actions

--- react/src/js/components/post/PostFooter.js
+++ react/src/js/components/post/PostFooter.js
@@ -1,38 +1,67 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+// import Heart from '../Heart';
 
-export default function PostFooter({ likes, isLiked, comments, favoriteImage }) {
-    return (
-        <footer>
-            <p className="postLikes">
-                {!likes ? null : `${likes} likes`}
-            </p>
-            {/* Comments */}
-            <div className="postComments">
-                {
-                    comments.map(comment => (
-                        <p key={comment.id}>
-                            <strong>{comment.username} </strong>
-                            {comment.text}
-                        </p>
-                    ))
-                }
-            </div>
-            <div className="postActions">
-                {/* Heart */}
-                {/* Comment input */}
-                {/* dot dot dot */}
-            </div>
-        </footer>
-    );
+export default class PostFooter extends Component {
+    static propTypes = {
+        comments: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.number,
+            text: PropTypes.string,
+            username: PropTypes.string
+        })).isRequired,
+        favoriteImage: PropTypes.func.isRequired,
+        isLiked: PropTypes.bool.isRequired,
+        likes: PropTypes.number.isRequired,
+        saveComment: PropTypes.func.isRequired,
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            commentInput: ''
+        };
+    }
+    handleCommentInputChange = (event) => {
+        this.setState({
+            commentInput: event.target.value
+        });
+    };
+    handleSave = (event) => {
+        if (event.key === 'Enter') {
+            this.props.saveComment(event);
+            this.setState({
+                commentInput: ''
+            });
+        }
+    };
+    render() {
+        const { likes, isLiked, comments, favoriteImage } = this.props;
+        return (
+            <footer>
+                <p className="postLikes">
+                    {!likes ? null : `${likes} likes`}
+                </p>
+                {/* Comments */}
+                <div className="postComments">
+                    {
+                        comments.map(comment => (
+                            <p key={comment.id}>
+                                <strong>{comment.username} </strong>
+                                {comment.text}
+                            </p>
+                        ))
+                    }
+                </div>
+                <div className="postActions">
+                    {/* Heart */}
+                    <input
+                      type="text"
+                      value={this.state.commentInput}
+                      onChange={this.handleCommentInputChange}
+                      placeholder="Add a comment..."
+                      onKeyPress={this.handleSave}
+                    />
+                    {/* dot dot dot */}
+                </div>
+            </footer>
+        );
+    }
 }
-
-PostFooter.propTypes = {
-    comments: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number,
-        text: PropTypes.string,
-        username: PropTypes.string
-    })).isRequired,
-    favoriteImage: PropTypes.func.isRequired,
-    isLiked: PropTypes.bool.isRequired,
-    likes: PropTypes.number.isRequired
-};
